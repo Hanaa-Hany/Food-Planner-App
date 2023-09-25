7bc4c9b93c144d095a09b8df602773a840fdb2a2
Hanaa Hany <hanaahany822@gmail.com>
2023-09-25 23:48:59 +0300

    Splach Screen done

---
+++ app/src/main/java/com/hanaahany/foodplannerapp/SplachScreenFragment.java
@@ -0,0 +1,72 @@
+package com.hanaahany.foodplannerapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.CompletableObserver;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+
+
+
+public class SplachScreenFragment extends Fragment {
+
+
+    private static final String TAG = "SplachScreenFragment";
+
+    NavController navController;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splach_screen, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //RXjava
+
+
+        Completable
+                .timer(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new CompletableObserver() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onComplete()
+                    {
+                        navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+                        navController.navigate(R.id.action_splachScreenFragment_to_loginFragment);
+                        Log.i(TAG, "onComplete: ");
+                    }
+
+
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+                });
+    }
+}
