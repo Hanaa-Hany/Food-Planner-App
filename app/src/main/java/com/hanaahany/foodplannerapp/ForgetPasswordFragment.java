23ffd1f12c89b7b21d569a7df891dc3194814a5c
Hanaa Hany <hanaahany822@gmail.com>
2023-09-26 20:41:37 +0300

    Forget Password & Sign up UI done

---
+++ app/src/main/java/com/hanaahany/foodplannerapp/ForgetPasswordFragment.java
@@ -0,0 +1,29 @@
+package com.hanaahany.foodplannerapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ForgetPasswordFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forget_password, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
