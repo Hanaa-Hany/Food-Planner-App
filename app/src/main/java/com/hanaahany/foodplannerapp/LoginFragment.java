5884d36095f18dc7d335f67ff99860f9e7702cd2
Hanaa Hany <hanaahany822@gmail.com>
2023-09-26 08:19:16 +0300

    Login Ui done

--- app/src/main/java/com/hanaahany/foodplannerapp/LoginFragment.java
+++ app/src/main/java/com/hanaahany/foodplannerapp/LoginFragment.java
@@ -5,13 +5,24 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class LoginFragment extends Fragment {
+
+    TextInputEditText textInputEditTextEmail,textInputEditTextPassword;
+    MaterialButton materialButtonLogin;
+    TextView textViewForgotPassword,textViewSignUp;
+    NavController navController;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,5 +34,25 @@ public class LoginFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initViews();
+        navController= Navigation.findNavController(getActivity(),R.id.nav_host_fragment);
+        onClicks();
+    }
+
+    private void onClicks() {
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_loginFragment_to_signUpFragment);
+            }
+        });
+    }
+
+    private void initViews() {
+        textInputEditTextEmail=getView().findViewById(R.id.et_email_login);
+        textInputEditTextPassword=getView().findViewById(R.id.et_password_login);
+        materialButtonLogin=getView().findViewById(R.id.btn_login);
+        textViewForgotPassword=getView().findViewById(R.id.tv_forget_password);
+        textViewSignUp=getView().findViewById(R.id.tv_sign_up);
     }
 }
